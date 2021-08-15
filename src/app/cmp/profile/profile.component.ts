bb90e4041262db17fd160d95563f66b62139ca5a
Aditya Thakur <aditya.thakur.261099@gmail.com>
2021-08-15 10:08:57 +0530

    feat (proto) : profile image selector

--- src/app/cmp/profile/profile.component.ts
+++ src/app/cmp/profile/profile.component.ts
@@ -7,11 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  private imgURL = '';
+
   public imgSelected: Boolean = false;
   public viewMode: Boolean = false;
+  public iconName: string = 'account_circle';
 
-  constructor() {}
+  constructor() {
+  
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
+
+  onEnter() {
+    if (!this.imgSelected)
+      this.iconName = 'edit';
+  }
+
+  onLeave() {
+    if (!this.imgSelected)
+      this.iconName = 'account_circle';
+  }
+
+  onClick() {
+    const inputRef = document.getElementById('profileInput');
+    inputRef?.click();
+  }
+
+  setProfile() {
+
+    const inputRef: any = document.getElementById('profileInput');
+    const profileImg: any = document.getElementById('profileImg');
+    const fileData = inputRef?.files[0];
+
+    this.imgURL = URL.createObjectURL(fileData);
+    profileImg.src = this.imgURL;
+    this.imgSelected = true;
+  }
 
 }
