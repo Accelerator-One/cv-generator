f2a12b3e52154b908a6db3589ee52b709eba2235
Aditya Thakur <aditya.thakur.261099@gmail.com>
2021-08-29 16:31:36 +0530

    feat (profile) : local store for avatar  image

--- src/app/cmp/profile/profile.component.ts
+++ src/app/cmp/profile/profile.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements AfterViewInit {
 
   private imgURL = '';
-
   public imgSelected: Boolean = false;
   public viewMode: Boolean = false;
   public iconName: string = 'account_circle';
 
-  constructor() {
+  constructor() { }
 
-  }
+  ngAfterViewInit() {
 
-  ngOnInit(): void {
+    const avatarLocal = window.localStorage.getItem('avatar');
+    const profileImg: any = document.getElementById('profileImg');
+
+    if (!!avatarLocal) {
+      profileImg.src = avatarLocal;
+      this.arrangeImage(false);
+      this.imgSelected = true;
+    }
 
   }
 
@@ -40,14 +46,43 @@ export class ProfileComponent implements OnInit {
 
     const inputRef: any = document.getElementById('profileInput');
     const profileImg: any = document.getElementById('profileImg');
-    const docHeight = (window.innerHeight * 28) / 100;
     const fileData = inputRef?.files[0];
 
     this.imgURL = URL.createObjectURL(fileData);
     profileImg.src = this.imgURL;
+    this.arrangeImage(true);
     this.imgSelected = true;
 
-    setTimeout(() => {
+  }
+
+  async saveLocally(width: number, height: number) {
+
+    let canvas = document.createElement('canvas');
+    const imgRef: any = document.getElementById('profileImg');
+
+    const canvasRef: any = canvas.getContext('2d');
+    canvas.width = width;
+    canvas.height = height;
+
+    canvasRef?.drawImage(imgRef, 0, 0);
+    await canvas.toBlob(async (res: any) => {
+
+      let reader = new FileReader();
+      await reader.readAsDataURL(res);
+      reader.onload = (res: any) => {
+        window.localStorage.setItem('avatar', res.target.result);
+      }
+
+    }, 'image/png', 0.8);
+
+  }
+
+  arrangeImage(flag: boolean) {
+
+    const profileImg: any = document.getElementById('profileImg');
+    const docHeight = (window.innerHeight * 28) / 100;
+
+    setTimeout(async () => {
 
       let width = profileImg.width;
       let height = profileImg.height;
@@ -62,6 +97,9 @@ export class ProfileComponent implements OnInit {
         profileImg.style.height = 'auto';
         profileImg.style.bottom = `${Math.abs(docHeight - profileImg.height) / 2}px`;
       }
+
+      if (flag === true)
+        await this.saveLocally(width, height);
 
     }, 200);
 
